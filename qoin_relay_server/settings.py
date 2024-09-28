42c1c6e0031fbbd4668cb75c1a1bd7d979e70334
kdukuray <kdukuray01@gmail.com>
2024-09-28 11:20:14 -0400

    added static root

--- qoin_relay_server/settings.py
+++ qoin_relay_server/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -116,7 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+# Heroku deployment requires statis root
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+###
 STATIC_URL = 'static/'
 
 # Default primary key field type
